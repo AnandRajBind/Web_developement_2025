aec3919c6a91cb58ce49c53becf47cb46f6fe8a4
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-03-10 12:19:51 +0530

    10/03/2025
    Password Generator Project by using the hooks like usestate(),useEffect(), useRef(),useCallback() etc.

--- React/05passwordGenerator/src/App.jsx
+++ React/05passwordGenerator/src/App.jsx
@@ -1,23 +1,37 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 import "./index.css";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
+
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
-  //in the  useCallback() hook  two parameter are  passed first is function another is dependency in array formate.
+  //in the  useCallback() hook  two parameter are  passed first is callback function another is dependency in array formate. useCallback() hook are used to optimization.
+  // ***********************useref hook**************************
+  const passwordRef = useRef(null);
+
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if (numberAllowed) str += "0123456789";
     if (charAllowed) str += "~!@#$%^&*(){}[]?<>=-_+/";
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char);
+      pass += str.charAt(char);
     }
     setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword]);
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+     passwordRef.current?.setSelectionRange(0,50);// copy value in the range 
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+  //**************************useEffect Hook are used when any eny element is changed then rerander the application.**************** */
+  //in the  useEffect() hook  two parameter are  passed first is callback function another is dependency in array formate.
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
 
   return (
     <>
@@ -33,20 +47,50 @@ function App() {
             className=" bg-white text-2xl text-gray-700 outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
+            ref={passwordRef}
           />
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+          <button
+            onClick={copyPasswordToClipboard}
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+          >
             copy
           </button>
         </div>
         <div className="flex text-sm gap-x-2">
           <div className=" flex items-center gap-x-1">
-            <input type="range"
-            min={6}
-            max={100}
-            value={length}
-            className="cursor-pointer"
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e)=>{
+                setLength(e.target.value);
+              }}
             />
             <label htmlFor="">Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="characterInput">Characters</label>
           </div>
         </div>
       </div>
