3057d6aa78efa11bff9469a3386e186f7ed5d391
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-03-26 16:46:12 +0530

    26/03/2025
    Nextjs Router

--- React/08miniContext/src/components/Profile.jsx
+++ React/08miniContext/src/components/Profile.jsx
@@ -1,8 +1,16 @@
 import React, { useState, useContext } from "react";
 import UserContext from '../context/UserContext';
 
+<<<<<<< HEAD
 function Profile() {
     const { user } = useContext(UserContext)
     if (!user) return <div> pleaselogin </div>
     return <div>Welcome{user.username}</div>
 } 
+=======
+export default function Profile() {
+  const { user } = useContext(UserContext)
+  if (!user) return <div> pleaselogin </div>
+  return <div>Welcome{user.username}</div>
+}
+>>>>>>> 01ae6ece1f058b0b80ceb393a2cf7df7ec4446a8
