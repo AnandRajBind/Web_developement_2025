577de47f8b93cb497574306e791496b974245c0c
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-09-09 19:01:49 +0530

    09/09/2025
    alter table

---
+++ SQL/alter table.sql
@@ -0,0 +1,9 @@
+-- select * from customers
+-- alter table customers add column is_active boolean default true;
+-- alter table customers drop column is_active;
+-- alter table customers modify column name varchar(100);
+-- alter table customers modify column email varchar(100) after id;
+-- alter table customers modify column date_of_birth datetime first;
+select * from customers
+
+
