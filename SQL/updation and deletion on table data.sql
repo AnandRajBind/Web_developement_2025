a850a102216c50218a16b8058fcc01e4d2177ae4
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-09-12 16:15:08 +0530

    12/09/2025

---
+++ SQL/updation and deletion on table data.sql
@@ -0,0 +1,11 @@
+use startersql;
+-- update users set salary=45000 where id=1;
+-- update users set salary=45000, email="arav5252@gmail.com" where id=1;
+ -- update users set salary=70000 where id=5;
+ -- update users set name="Anand Raj" where email="raj@example.com";
+ -- update users set salary=salary+10000 where salary <60000 and id >0;
+
+-- delete from users where id=3;
+-- delete from users where salary<75000 and id>0;
+ -- drop table users;
+select *from users;
