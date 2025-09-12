a0cde0e06d1c9a3a851eb3cf89f7bccd8192b409
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-09-12 19:29:56 +0530

    12/09/2020

---
+++ SQL/constraints and Alter Table.sql
@@ -0,0 +1,12 @@
+-- create database startersql;
+-- use startersql;
+-- it can t be possible because email is unique. aisha@example.com email is already exist in the table.
+-- insert into users (name,email, gender,date_of_birth,salary) values ("Aarav","aisha@example.com","Male","1995-05-14",65000);
+-- alter table users add constraint unique_email unique(email);
+-- alter table users modify column name varchar(100) null;
+-- Check constraints 
+alter table users add constraint chk_dob check(date_of_birth > "1920-01-01");
+insert into users (name,email, gender,date_of_birth,salary) values ("Aarav","aisha2@example.com","Male","1905-05-14",65000);
+
+select * from users;
+ 
--- SQL/updation and deletion on table data.sql
+++ SQL/updation and deletion on table data.sql
@@ -8,4 +8,5 @@ use startersql;
 -- delete from users where id=3;
 -- delete from users where salary<75000 and id>0;
  -- drop table users;
+ 
 select *from users;
