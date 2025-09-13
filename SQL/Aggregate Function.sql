2595a2bdb83f7aa34c843669226a383d8c48ffb7
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-09-13 18:10:08 +0530

    13/09/2025
    Aggregate Function

---
+++ SQL/Aggregate Function.sql
@@ -0,0 +1,22 @@
+use startersql;
+select * from users;
+-- select count(*) from users;
+-- select count(*) from users where gender="Male";
+
+-- select min(salary) as min_salary , max(salary) as max_salary from users;
+-- select avg(salary) as avg_salary from users;
+-- select sum(salary) as total_salary from users;
+-- select gender, avg(salary) as avg_salary from users  group by gender;
+-- select gender, sum(salary) as total_salary from users  group by gender;
+
+
+-- String function 
+-- select name, LENGTH(name) as name_len from users;
+-- select name, id, LENGTH(name) as name_len from users;
+-- select id,name,gender, lower(name) as lower_name, LENGTH(name) as name_len from users;
+-- select id,name,  gender, lower(name) as lower_name, concat(lower(name), "5677") as username, now() as time, year(date_of_birth) as YOB, LENGTH(name) as name_len from users;
+-- select id,name,  gender, lower(name) as lower_name, concat(lower(name), "5677") as username, now() as time, year(date_of_birth) as YOB,day(date_of_birth) as day,month(date_of_birth) as month, LENGTH(name) as name_len from users;
+select name, datediff(curdate(), date_of_birth) as days from users;
+
+ 
+ 
--- SQL/constraints and Alter Table.sql
+++ SQL/constraints and Alter Table.sql
@@ -9,4 +9,3 @@ alter table users add constraint chk_dob check(date_of_birth > "1920-01-01");
 insert into users (name,email, gender,date_of_birth,salary) values ("Aarav","aisha2@example.com","Male","1905-05-14",65000);
 
 select * from users;
- 
