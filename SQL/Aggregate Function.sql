9f84586e80406c84e716620e6762c7eadfd4b418
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-09-13 18:34:55 +0530

    13/09/2025
    Set AutoCommit, RollBack and commit

--- SQL/Aggregate Function.sql
+++ SQL/Aggregate Function.sql
@@ -16,7 +16,11 @@ select * from users;
 -- select id,name,gender, lower(name) as lower_name, LENGTH(name) as name_len from users;
 -- select id,name,  gender, lower(name) as lower_name, concat(lower(name), "5677") as username, now() as time, year(date_of_birth) as YOB, LENGTH(name) as name_len from users;
 -- select id,name,  gender, lower(name) as lower_name, concat(lower(name), "5677") as username, now() as time, year(date_of_birth) as YOB,day(date_of_birth) as day,month(date_of_birth) as month, LENGTH(name) as name_len from users;
-select name, datediff(curdate(), date_of_birth) as days from users;
-
+-- select name, datediff(curdate(), date_of_birth) as days from users;
+SELECT salary,
+       ROUND(salary) AS rounded,
+       FLOOR(salary) AS floored,
+       CEIL(salary) AS ceiled
+FROM users;
  
  
---
+++ SQL/Auto commit & Transactions.sql
@@ -0,0 +1,8 @@
+use startersql;
+
+set autocommit=0;
+select *from users;
+-- rollback;
+ delete from users where id=7;
+ commit;
+ select *from users;
