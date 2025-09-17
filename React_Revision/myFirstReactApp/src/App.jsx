625fcba6c7dcfe85c4328cec539623acdad9e811
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-09-17 11:58:16 +0530

    17/09/2025
    Embedding expression in jsx.
    simple Expression
    functional expression
    conditional expression

--- React_Revision/myFirstReactApp/src/App.jsx
+++ React_Revision/myFirstReactApp/src/App.jsx
@@ -1,11 +1,39 @@
+// Embedding expressions in JSX
 import './App.css'
 
+
 function App() {
- 
-  var element= <h1>Anand Raj Bind</h1>
+  //   Simple expression 
+  var element = <h1>I am Simple Expression </h1>
+
+  //  functional expression 
+  const elementB = ()=>{
+    return(
+      <div>
+        <h1>This is functional expression</h1>
+      </div>
+    );
+  }
+  const elementC= () => {
+    return(
+      <div>
+        <h1>I am conditional renderiong </h1>
+      </div>
+    )
+  }
+let num=10;
+
+
   return (
-    <> 
-{element}
+    <>
+      {/* simple expression call */}
+      {element}
+
+      {/* functional expression call */}
+      {/* {elementB()} */}
+
+      {/* conditional rendering */}
+      {num%2 == 0? elementB():elementC()}
 
     </>
   )
