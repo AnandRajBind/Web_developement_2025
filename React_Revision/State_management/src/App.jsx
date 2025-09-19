7d683072e7a8b62d8561fe446798a9941d0ef305
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-09-19 23:23:45 +0530

    19/09/2025
    complete counter App and used to inline, internal and External CSS

--- React_Revision/State_management/src/App.jsx
+++ React_Revision/State_management/src/App.jsx
@@ -3,33 +3,54 @@ import React, { useState } from 'react'
 
 
 function App() {
+
+
+  // internal css
+  const heading = {
+    color: "red",
+    backgroundColor: "black",
+    padding: "10px",
+    borderRadius: "10px",
+  }
+
   const [data, setdata] = useState(0);
+  const [name, setname] = useState("codingHunger");
 
   // const handleClick = () => {
   //   setdata(data + 1)
   // }
-const handleAddition=() => {
-  setdata(data + 1)
-}
-const handleSubstraction=() =>{
-  setdata(data - 1)
-}
+  const handleAddition = () => {
+
+    // setdata(data + 1)// it is not store the previous value add directly
+
+    // another way to write/update the  function value
+    setdata(prev => prev + 1)// it is store the previous value and add 1
+
+    setname("It is Increasing......... value ")
+  }
+  const handleSubstraction = () => {
+    // setdata(data - 1)
+
+    // correct way to write/update the function
+    setdata(prev => prev - 1)
+    setname("It is Decreasing......... value ")
+  }
 
   return (
     <>
       <div>
-      {/* <div onClick={ handleClick }> */} 
+        {/* <div onClick={ handleClick }> */}
         {/* <h1>{data}</h1> */}
-
-
-
         <div >
-          <h1> Counter   App  {data} </h1>
-          <button onClick={handleAddition}>Addition + </button>
-          <button onClick={handleSubstraction}>Substraction -
+          {/* use internal css */}
+          <h1 style={heading}> Counter   App  {data} </h1>
+          <div class="codingHunger">{name}</div>
+          {/* inline css used */}
+          <button style={{ backgroundColor: "aqua", margin: 5 }} onClick={handleAddition}>Addition + </button>
+          <button style={{ backgroundColor: "aqua", margin: 5 }} onClick={handleSubstraction}>Substraction -
 
           </button>
-          </div>
+        </div>
       </div>
     </>
   )
