581504e12c66bdc813819de580756439b4c99e0a
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-09-28 22:03:48 +0530

    28/09/2025
    Page not found errors solve (random url problem solve)

--- React_Revision/Routing/src/App.jsx
+++ React_Revision/Routing/src/App.jsx
@@ -1,6 +1,7 @@
 
 import { Home } from './component/Home'
 import { Navbar } from './component/Navbar'
+import { NotFound } from './component/NotFound'
 import { Profile } from './component/Profile'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
@@ -12,6 +13,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='profile' element={<Profile />} />
+          {/* It is must be last Routs  */}
+          <Route path='*' element={<NotFound />} /> 
+        
         </Routes>
       </>
     </Router>
