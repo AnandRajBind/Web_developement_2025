75f60ec2dce5b248d79800228ca4905a2b26d1e7
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-11-22 16:50:55 +0530

    22/11/2025
    Chatting Application Completed

--- Scoket_IO/Chat-App/index.js
+++ Scoket_IO/Chat-App/index.js
@@ -1,11 +1,28 @@
-const express=require('express');
-const http=require('http');
-const app=express();
+// npm  install express socket.io
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io'); // Importing Server from socket.io
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);// Creating a new Socket.io server instance
 
-const server=http.createServer(app);
+// Socket.io connection.  
+// here socket represents each connected client/User.
+io.on("connection", (socket) => {
+    // console.log("New user connected", socket.id);
+    socket.on("user-message", (message) => {
+        // Broadcasting the message to all connected clients including sender
+        console.log("A new User Message", message);
+        io.emit("message", message); // Emitting or send message event to all clients. takes the message from frontend and send to all clients
+    });
+});
 
-app.use(express.static('public'));
+app.use(express.static(path.resolve("./public")));
+app.get('/', (req, res) => {
+    return res.sendFile("index.html");
+})
 
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
