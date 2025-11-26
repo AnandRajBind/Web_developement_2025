aba889821210f0c55a3dda9e3632bc8950949a43
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-11-26 17:02:05 +0530

    26/11/2025
    Make project setup and background.

---
+++ MultiPlayer Game/Tic-Tac-Toe/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div class='main-div'>
+      <div>
+        <h1>Tic Tac Toe</h1>
+      </div>
+
+      </div>
+  )
+}
+
+export default App
