1e910d8b5a45048576155bf185624d559ddc2e18
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-11-26 22:35:27 +0530

    26/11/2025
    Tic Tac Toe Squire completed

--- MultiPlayer Game/Tic-Tac-Toe/src/App.jsx
+++ MultiPlayer Game/Tic-Tac-Toe/src/App.jsx
@@ -1,15 +1,30 @@
 
 import React from 'react'
+import Squire from './Square/Squire'
 import './App.css'
+
+// Dummy data for rendering squares
+const renderFrom = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 function App() {
   return (
-    <div class='main-div'>
-      <div>
-        <h1>Tic Tac Toe</h1>
+    <div className='main-div'>
+       <div className='move-detection'>
+          <div className="left">YourSelf</div>
+          <div className="right">Opponent</div>
+        </div>
+      <div >
+        <h1 className='game-heading water-background'>Tic Tac Toe</h1>
+        <div className='square-wrapper'>
+          {
+            renderFrom.map((arr) =>
+              arr.map((e) => {
+                return <Squire />
+              })
+            )}
+        </div>
       </div>
-
-      </div>
+    </div>
   )
 }
 
---
+++ MultiPlayer Game/Tic-Tac-Toe/src/Square/Squire.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Square.css'
+
+function Squire() {
+  return (
+    <div className='square'></div>
+  )
+}
+
+export default Squire
