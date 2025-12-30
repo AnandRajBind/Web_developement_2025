ec457091793b0571d9cdd9856110b05db8783faa
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2025-12-30 20:04:59 +0530

    30/12/2025

--- MultiPlayer Game/Tic-Tac-Toe/src/App.jsx
+++ MultiPlayer Game/Tic-Tac-Toe/src/App.jsx
@@ -2,24 +2,30 @@
 import React from 'react'
 import Squire from './Square/Squire'
 import './App.css'
+import { useState } from 'react'
 
 // Dummy data for rendering squares
 const renderFrom = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
+const [gameState, setGameState] = useState(renderFrom)
+
 function App() {
   return (
     <div className='main-div'>
-       <div className='move-detection'>
-          <div className="left">YourSelf</div>
-          <div className="right">Opponent</div>
-        </div>
+      <div className='move-detection'>
+        <div className="left">YourSelf</div>
+        <div className="right">Opponent</div>
+      </div>
       <div >
         <h1 className='game-heading water-background'>Tic Tac Toe</h1>
         <div className='square-wrapper'>
           {
-            renderFrom.map((arr) =>
+            gameState.map((arr) =>
               arr.map((e) => {
-                return <Squire />
+                return <Squire
+                  setGameState={setGameState}
+                  key={e}
+                  value={e} />
               })
             )}
         </div>
